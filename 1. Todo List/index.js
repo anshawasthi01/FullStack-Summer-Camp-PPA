a84ae5be1becf49b5838ba72bb3e72a03bc718dc
anshawasthi01 <anshawasthi3894@gmail.com>
2023-07-18 01:15:12 +0530

    ToDo List Add Functionality Using Vanilla JS

--- 1. Todo List/index.js
+++ 1. Todo List/index.js
@@ -1,5 +1,7 @@
 let inputTag = document.getElementsByTagName("input")[0];
+// as there can be multiple tag it choose first tag
 let todo = document.getElementsByClassName("list")[0];
+// multiple element can share class name, will grab first element from that list
 
 function addnewtodo() {
   if (inputTag.value.length < 1) {
